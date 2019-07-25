a10c71de655ed3f0dc22c71692b7d9971a2e282c
yangshuai <amosyang@shunshunliuxue.com>
2019-07-25 15:46:45 +0800

    bigpipe demp

--- node/bigpipe-demo/app.js
+++ node/bigpipe-demo/app.js
@@ -1,5 +1,7 @@
 const koa = require('koa');
 const Router = require('koa-router');
+const fs = require('fs');
+const { resolve } = require('path');
 
 const app = new koa();
 const router = new Router();
@@ -7,7 +9,7 @@ const router = new Router();
 const task1 = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve('第一次传输<br/>');
+            resolve(`<script>addHtml('part1', '第一次传输')</script>`);
         }, 1000);
     });
 }
@@ -15,8 +17,8 @@ const task1 = () => {
 const task2 = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve('第二次传输<br/>');
-        }, 2000);
+            resolve(`<script>addHtml('part2', '第二次传输')</script>`);
+        }, 1000);
     });
 }
 
@@ -24,15 +26,34 @@ router.get('/', async(ctx, next) => {
     // ctx.body = 123;
     ctx.status = 200;
     ctx.type = 'html';
-    ctx.res.write('loading... <br/>');
 
-    const result = await task1();
+    // 不采用流的方式 读完再吐 用于读的文件较小
+    // const file = fs.readFileSync('./index.html', 'utf-8'); // 如果这个文件较大我们采用流的方式 边读边吐
+    // ctx.res.write(file);
 
-    ctx.res.write(result);
+    // 采用流的方式 边读边吐
+    const stream = fs.createReadStream(resolve(__dirname, 'index.html'));
+    // stream.pipe(ctx.res); // 此种方法pipe完后会执行ctx.res.end(); 故它适合单个文件读写的情景。不适合分块~~
+    stream.on('data', chunk => { // 一段一段的写进页面
+        ctx.res.write(chunk);
+    })
 
-    const result2 = await task2();
+    // 并行执行操作 task1 和 task2 同时返回数据
+    await Promise.all([task1().then(r => {
+        ctx.res.write(r);
+    }), task2().then(r => {
+        ctx.res.write(r);
+    })]);
 
-    ctx.res.write(result2);
+    // const result = await task1();
+
+    // ctx.res.write(result);
+
+    // const result2 = await task2();
+
+    // ctx.res.write(result2);
+
+    ctx.res.write(`</body></html>`); // 把页面合上
     ctx.res.end();
 });
 
